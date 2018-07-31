e0a3dd3f5da0f6e14eeebb80defe4d8f92b511e8
Nairi Harutyunyan <nairihar99@gmail.com>
2018-07-31 18:16:26 +0400

    change folder name

--- Part 2/with_redux/src/index.js
+++ Part 2/with_redux/src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux';
 
 import reducer from './reducer';
-import App from './components/App';
+import App from './containers/App';
 
 const store = createStore(reducer)
 
